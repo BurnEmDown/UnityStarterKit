349c0331d3f0d73a6cbc591ce57d63ff86fdd851
Amit Reuveni <amitreuveni@gmail.com>
2025-11-19 03:07:55 +0200

    fixed namespace for openloadertool

--- Assets/Scripts/Editor/OpenLoaderTool.cs
+++ Assets/Scripts/Editor/OpenLoaderTool.cs
@@ -3,22 +3,25 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
-public class OpenLoaderTool
+namespace Editor
 {
-    [MenuItem("COD/PlayGame")]
-    //[System.Obsolete]
-    public static void OpenLoader()
+    public class OpenLoaderTool
     {
-        string currentSceneName = SceneManager.GetActiveScene().name;
-        File.WriteAllText(".lastScene", currentSceneName);
-        EditorSceneManager.OpenScene($"{Directory.GetCurrentDirectory()}/Assets/Scenes/InitScene.unity");
-        EditorApplication.isPlaying = true;
-    }
+        [MenuItem("COD/PlayGame")]
+        //[System.Obsolete]
+        public static void OpenLoader()
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            File.WriteAllText(".lastScene", currentSceneName);
+            EditorSceneManager.OpenScene($"{Directory.GetCurrentDirectory()}/Assets/Scenes/InitScene.unity");
+            EditorApplication.isPlaying = true;
+        }
 
-    [MenuItem("COD/LoadEditedScene")]
-    public static void ReturnToLastScene()
-    {
-        string lastScene = File.ReadAllText(".lastScene");
-        EditorSceneManager.OpenScene($"{Directory.GetCurrentDirectory()}/Assets/Scenes/{lastScene}.unity");
+        [MenuItem("COD/LoadEditedScene")]
+        public static void ReturnToLastScene()
+        {
+            string lastScene = File.ReadAllText(".lastScene");
+            EditorSceneManager.OpenScene($"{Directory.GetCurrentDirectory()}/Assets/Scenes/{lastScene}.unity");
+        }
     }
 }
